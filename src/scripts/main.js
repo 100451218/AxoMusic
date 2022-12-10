fcb9424bc84896406982108e16936e7c00f6d200
100451058 <100451058@alumnos.uc3m.es>
2022-12-10 19:15:38 +0100

    Cambios en main y añadidos los botones de amigos y cuenta

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+
+window.addEventListener('load', (event) => {
+    if (localStorage.currentUser == "" | localStorage.currentUser == undefined){
+        //Poner aquí todos los elementos que desaparezcan cuando no haya usuario registrado
+        $("#button_amigos").hide();
+        $("#button_cuenta").hide();
+    }
+    else {
+        //Poner aquí todos los elementos que desaparezcan cuando haya un usuario registrado
+        $("#button_register").hide();
+
+    }
+});
