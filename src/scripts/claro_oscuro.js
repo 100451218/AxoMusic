a18ca8d2a44904a9447875e9aab11b7dde21cdd2
David <100460314@alumnos.uc3m.es>
2022-12-15 12:13:21 +0100

    claro y oscuro mantiene

--- src/scripts/claro_oscuro.js
+++ src/scripts/claro_oscuro.js
@@ -3,9 +3,20 @@ const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
 function cambiaTema(ev){
     if(ev.target.checked){
         document.documentElement.setAttribute('tema', 'dark');
+        localStorage.setItem("claro_oscuro", 1);
     } else {
         document.documentElement.setAttribute('tema', 'light');
+        localStorage.setItem("claro_oscuro", 0);
     }
 }
 colorSwitch.addEventListener('change', cambiaTema);
+
+color=localStorage.getItem("claro_oscuro")
+if (color==1) {
+    colorSwitch.checked=true
+    document.documentElement.setAttribute('tema', 'dark');
+
+}
+else
+    document.documentElement.setAttribute('tema', 'light');
 
