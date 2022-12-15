8e532adb0ce1f74d7a17154b9e2844aff7ea60b2
100451058 <100451058@alumnos.uc3m.es>
2022-12-15 10:28:32 +0100

    Cambios en main y botones superiores

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,17 +2,26 @@
 window.addEventListener('load', (event) => {
     if (localStorage.currentUser == "" | localStorage.currentUser == undefined){
         //Poner aquí todos los elementos que desaparezcan cuando no haya usuario registrado
-        $("#button_amigos").hide();
-        $("#button_cuenta").hide();
+        $("#a_cuenta").hide();
     }
     else {
         //Poner aquí todos los elementos que desaparezcan cuando haya un usuario registrado
-        $("#button_register").hide();
+        $("#a_register").hide();
 
     }
     document.getElementById("button_home").style.border = "5px solid #f57c00"
 
 });
 
+document.getElementById("button_playlist").addEventListener("click", function(e){not_log_in(e)});
+document.getElementById("button_amigos").addEventListener("click", function(e){not_log_in(e)});
+
+function not_log_in(e){
+    if (localStorage.currentUser == "" | localStorage.currentUser == undefined){
+        e.preventDefault()
+        alert("Inicia sesión para acceder")
+    }
+    return
+}
 
 
