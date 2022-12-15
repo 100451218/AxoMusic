e26614342863ae29c9afe1bfa30e6b343a9a4da6
100451058 <100451058@alumnos.uc3m.es>
2022-12-15 21:48:12 +0100

    Cambiado el modo de reproducir una canción

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,6 +11,7 @@ window.addEventListener('load', (event) => {
     }
     document.getElementById("button_home").style.border = "5px solid #f57c00"
     add_song()
+    add_song_not_listened()
 });
 
 document.getElementById("button_playlist").addEventListener("click", function(e){not_log_in(e)});
@@ -24,25 +25,23 @@ function not_log_in(e){
     return
 }
 
-document.getElementsByClassName("music")[0].addEventListener("click", function(){play_song()})
+document.getElementsByClassName("music")[0].addEventListener("click", function(e){play_song(e)})
+document.getElementsByClassName("sorprendeme")[0].addEventListener("click", function(e){play_song(e)})
 
 function add_song(){
     var key = "035.mp3"
     var music = document.getElementsByClassName("music")[0];
 
     //Para añadir la barra de reproducción
-    var music_controls = document.createElement("audio");
+    /*var music_controls = document.createElement("audio");
     music_controls.setAttribute("controls", "");
     music_controls.setAttribute("class", "music_control");
-    var music_source = document.createElement("source");
-    music_source.setAttribute("src", "music/"+key);
-    music_source.setAttribute("type", "audio/mpeg");
-    music_controls.appendChild(music_source)
+    music_controls.setAttribute("id", "music_"+key)*/
     //Hasta aquí
 
     var img = document.createElement('img');
     img.setAttribute('src', "images/"+MUSIC_DATA[key][4]);
-    img.setAttribute("id", "music_image")
+    img.setAttribute("class", "music_image")
     var song_title = document.createElement('h4');
     song_title.innerHTML=MUSIC_DATA[key][0];
     var song_autor = document.createElement('p');
@@ -50,10 +49,37 @@ function add_song(){
     music.appendChild(img);
     music.appendChild(song_title);
     music.appendChild(song_autor);
-    music.appendChild(music_controls);
+    music.setAttribute('id',"music_"+key);
+}
+
+function add_song_not_listened(){
+    var key = "038.mp3"
+    var music = document.getElementsByClassName("sorprendeme")[0];
+
+    //Para añadir la barra de reproducción
+    /*var music_controls = document.createElement("audio");
+    music_controls.setAttribute("controls", "");
+    music_controls.setAttribute("class", "music_control");
+    music_controls.setAttribute("id", "music_"+key)*/
+    //Hasta aquí
+
+    var img = document.createElement('img');
+    img.setAttribute('src', "images/"+MUSIC_DATA[key][4]);
+    img.setAttribute("class", "music_image")
+    var song_title = document.createElement('h4');
+    song_title.innerHTML=MUSIC_DATA[key][0];
+    var song_autor = document.createElement('p');
+    song_autor.innerHTML=MUSIC_DATA[key][1];
+    music.appendChild(img);
+    music.appendChild(song_title);
+    music.appendChild(song_autor);
+    music.setAttribute('id',"music_"+key);
 }
 
 function play_song(e){
-
+    info_audio = e.target.parentElement.id;
+    source_url = info_audio.split("_")[1];
+    document.getElementById("music_control").setAttribute("src", "music/"+source_url)
+    $("#music_control").show()
 }
 
