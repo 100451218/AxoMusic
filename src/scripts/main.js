22dfd6dcbc2694d9224babdd81dafc74afa1a7e5
David <100460314@alumnos.uc3m.es>
2022-12-16 08:49:27 +0100

    me gusta en proceso

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -84,9 +84,54 @@ function play_song(e){
     if (object_to_change == source_url){
         document.getElementById("music_control").setAttribute("src", "")
         $("#music_control").hide()
+        $(".me_gusta_button").hide()
         return
     }
     document.getElementById("music_control").setAttribute("src", source_url)
     $("#music_control").show()
+    if (localStorage.currentUser != "" && localStorage.currentUser != undefined){  
+        $(".me_gusta_button").show() // para que enseñe el botón de me gusta solo a users
+    }
+    
 }
 
+function agregar_a_me_gusta(){
+    //var db = JSON.parse(db_string);
+    var audio_source = document.getElementById('music_control').getAttribute('src'); //coger la direccion de la canción
+
+    var user = localStorage.getItem("current_user");
+    var isitempty = localStorage.getItem(user + "_me_gusta");
+
+  /*var longitud = audio_source.length - 4;
+    var audio_source = audio_source.substring(7, longitud);*/
+
+
+    if (isitempty=="0" | isitempty==undefined) {
+        localStorage.setItem(user + "_me_gusta","1");
+        name = "me gusta";
+
+    }
+    //console.log(audio_source);
+
+
+
+    
+    image_path =  audio_source + ".jpg";
+    playsong_path = "images/" + audio_source + ".mp3";
+    mp3 = audio_source;
+
+
+    for (const [key, value] of Object.entries(db)){
+        var songs = db[key];
+        for(var i = 0; i < songs.length; i++){
+            if(songs[i].path == image_path){
+                var name = songs[i].name;
+                continuar();
+                break;
+            }
+    
+        }
+    }
+    
+}
+    
