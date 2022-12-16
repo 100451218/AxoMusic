626fe1d89ffa1ac87d8beb78b107894c5e4ac6ec
Sergi <100451218@alumnos.uc3m.es>
2022-12-16 02:00:10 +0100

    Crea con éxito las playlists

--- src/scripts/crear_playlist.js
+++ src/scripts/crear_playlist.js
@@ -1,9 +1,32 @@
+function add_song_to_playlist(){
+    //En this tendremos el objeto que el usuario quiere añadir a la playlist
+    //Por ahora, lo añade a localStorage y la oculta, nada más.
+    var user=localStorage.getItem('currentUser');
+    var nombre_nueva_playlist = document.getElementById('nombre_nueva_playlist').value;
+    console.log(nombre_nueva_playlist)
+    if (nombre_nueva_playlist.value==""){
+        alert('No se puede añadir a playlist sin nombre')
+    } else if (localStorage.getItem(user+'_playlist_'+nombre_nueva_playlist)==null) {
+        //Si el nombre es válido pero no existe la canción
+        localStorage.setItem(user+'_playlist_'+nombre_nueva_playlist, this.id);
+        this.style.display="none";
+    } else{
+        //Si han puesto un nombre válido y ya existe esta playlist, sacamos primero las canciones
+        var canciones = localStorage.getItem(user+'_playlist_'+nombre_nueva_playlist);
+        console.log(canciones)
+        canciones = canciones+'|'+ this.id;
+        console.log(canciones)
+        localStorage.setItem(user+'_playlist_'+nombre_nueva_playlist, canciones);
+        this.style.display="none";
+    }
 
 
-function mostrar_add(){
-    //Ahora, cada vez que hoveremos este objeto (obtenible con this) podemos hacer que aparezca el icono de más
+
+
 
 }
+
+
 function load_add(){
     var all_elements=document.getElementsByTagName('li')
     for (var i=0; i<all_elements.length; i++){
@@ -14,6 +37,9 @@ function load_add(){
         image.style.width='100%';
         image.style.zIndex="-1";
         all_elements[i].appendChild(image);
+
+        //También queremos que se añada la canción al ser pulsada (y se oculte para evitar canciones repetidas).
+        all_elements[i].addEventListener('click', add_song_to_playlist);
     }
 }
 
