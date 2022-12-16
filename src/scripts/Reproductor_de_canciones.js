9b499a1e6d803a076973cf10b034222f2a93118c
100451058 <100451058@alumnos.uc3m.es>
2022-12-16 18:36:30 +0100

    Cambiado el nombre de index

--- src/scripts/Reproductor_de_canciones.js
+++ src/scripts/Reproductor_de_canciones.js
@@ -90,8 +90,7 @@ function reproducir_cola(cola, index){
     if (localStorage.currentUser != "" && localStorage.currentUser != undefined){
         $(".me_gusta_button").show() // para que enseñe el botón de me gusta solo a users
         localStorage.setItem(localStorage.getItem("currentUser")+"_escuchando", all_songs[index])
-        localStorage.setItem(localStorage.getItem("currentUser")+"_escuchando_playlist", added_cola)
-        localStorage.setItem(localStorage.getItem("currentUser")+"_playlist_index", parseInt(index))
+        localStorage.setItem(localStorage.getItem("currentUser")+"_escuchando_playlist", added_cola)localStorage.setItem(localStorage.getItem("currentUser")+"_listening_index", parseInt(index))
     }
 
     play_song(all_songs[index]);
@@ -101,12 +100,12 @@ function reproducir_cola(cola, index){
 window.addEventListener('load', () => {
     document.getElementById("button_prev_song").addEventListener("click", function () {
         anterior_cancion(localStorage.getItem(localStorage.getItem("currentUser") + "_escuchando_playlist"),
-            localStorage.getItem(localStorage.getItem("currentUser") + "_playlist_index"))
+            localStorage.getItem(localStorage.getItem("currentUser") + "_listening_index"))
     })
     document.getElementById("button_next_song").addEventListener("click", function () {
 
         siguiente_cancion(localStorage.getItem(localStorage.getItem("currentUser") + "_escuchando_playlist"),
-            localStorage.getItem(localStorage.getItem("currentUser") + "_playlist_index"))
+            localStorage.getItem(localStorage.getItem("currentUser") + "_listening_index"))
     })
 })
 
